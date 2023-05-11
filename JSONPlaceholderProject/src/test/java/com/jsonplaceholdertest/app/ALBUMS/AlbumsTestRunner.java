f1fbbdf0a5f61fae6a6a1030348bf6d55889afcb
PeaceGusenga <gusengapeace@gmail.com>
2023-05-11 16:07:45 +0200

    albums api tests

--- JSONPlaceholderProject/src/test/java/com/jsonplaceholdertest/app/ALBUMS/AlbumsTestRunner.java
+++ JSONPlaceholderProject/src/test/java/com/jsonplaceholdertest/app/ALBUMS/AlbumsTestRunner.java
@@ -4,7 +4,22 @@ import com.intuit.karate.junit5.Karate;
 
 public class AlbumsTestRunner {
     @Karate.Test
-    Karate testCreateComment() {
+    Karate testCreateAlbum() {
         return Karate.run("create-albums.feature").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testRetrieveAlbum() {
+        return Karate.run("retrieve-albums.feature").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testUpdateAlbum() {
+        return Karate.run("update-albums.feature").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testDeleteAlbum() {
+        return Karate.run("delete-albums.feature").relativeTo(getClass());
     }
 }
--- JSONPlaceholderProject/src/test/java/com/jsonplaceholdertest/app/ALBUMS/delete-albums.feature
+++ JSONPlaceholderProject/src/test/java/com/jsonplaceholdertest/app/ALBUMS/delete-albums.feature
@@ -1,3 +1,10 @@
-Feature:
+Feature: Test /albums endpoint
 
-Scenario:
+Background:
+  * url 'https://jsonplaceholder.typicode.com'
+
+
+Scenario: Delete an existing album
+    Given path '/albums/1'
+    When method delete
+    Then status 200
